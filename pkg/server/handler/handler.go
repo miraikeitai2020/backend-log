8e1939fcf135d79a05bf32286cdb4fc91620370c
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2020-10-03 12:01:03 +0900

    [add]README.mdに記述を追加

--- pkg/server/handler/handler.go
+++ pkg/server/handler/handler.go
@@ -1,12 +1,3 @@
 package handler
 
-import "github.com/gin-gonic/gin"
 
-func HandleLogCreate() gin.HandlerFunc{
-	return func(c *gin.Context){
-		// ここに処理を書いていく
-		c.JSON(200, gin.H{
-			"log_id":"asdfghjkl",
-		})
-	}
-}
